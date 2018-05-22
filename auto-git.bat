d92c246d7635dcda56deaa508349c6661222408d
fullarray <sifreed77@gmail.com>
2018-05-21 23:02:14 -0400

    Added new code 05212018_230214

--- auto-git.bat
+++ auto-git.bat
@@ -20,7 +20,7 @@ git commit -m "Added new code %Timestamp%"
 git push origin develop
 ::###########################
 ::echo %CommitMessage% 
-timeout /t 15
+timeout /t 60
 goto loop
 echo Press Enter...
 
