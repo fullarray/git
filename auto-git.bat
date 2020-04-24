b4ffceff1d3c341e0f7b57e2788710035e27776a
fullarray <sifreed77@gmail.com>
2020-04-23 23:10:18 -0400

    Updated date on header

--- auto-git.bat
+++ auto-git.bat
@@ -2,7 +2,7 @@
 :: Author: fullarray
 :: Description: Auto commit tool every 180 seconds. 
 :: License: MIT License 
-:: Copyright 2010-2020 fullarray
+:: Copyright 2012-2020 fullarray
 ::############################
 @echo off
 set UserInputPath=%cd%
